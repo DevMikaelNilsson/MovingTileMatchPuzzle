a5932bf6c5aae220853dc09dffacca29746f3ff0
DevMikaelNilsson <DevMikaelNilsson@outlook.com>
2016-02-11 07:58:59 +0100

    Update Bezier.cs

--- Scripts/Moving-Line/BezierCurve/Bezier.cs
+++ Scripts/Moving-Line/BezierCurve/Bezier.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 	This code is based on the tutorial and source code from Catlike Coding.
 	Link: http://catlikecoding.com/unity/tutorials/curves-and-splines/
 */
-namespace TileMatch.MovingLine
+namespace TileMatch.MovingLine.BezierCurve
 {
 	public static class Bezier 
 	{
