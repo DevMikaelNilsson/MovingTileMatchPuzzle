869b141257ca430a5bea410a5676c259cde4a5cc
DevMikaelNilsson <DevMikaelNilsson@outlook.com>
2016-02-11 07:58:50 +0100

    Create BezierControlPointMode.cs

---
+++ Scripts/Moving-Line/BezierCurve/BezierControlPointMode.cs
@@ -0,0 +1,13 @@
+/*
+	This code is based on the tutorial and source code from Catlike Coding.
+	Link: http://catlikecoding.com/unity/tutorials/curves-and-splines/
+*/
+namespace TileMatch.MovingLine.BezierCurve
+{
+	public enum BezierControlPointMode 
+	{
+		Free,
+		Aligned,
+		Mirrored
+	}
+}
